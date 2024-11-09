b657a30b808c84cdedd8cfc11d4656ef99036fb3
KamilTPWR <277985@student.pwr.edu.pl>
2024-11-09 16:42:40 +0100

    pierwsza działajaca baza

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ZarządzanieFinansami;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        StartClock();
+
+        this.ResultTextDisplay.Text = "Some string";
+        
+        //Zarządzanie zegarem w prawym górnym rogu
+        void StartClock()
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += TickEvent;
+            timer.Start();
+        }
+        void TickEvent(object sender, EventArgs e)
+        {
+            SystemClock.Text = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+        }
+    }
+}
---
+++ Transaction.cs
@@ -0,0 +1,19 @@
+﻿
+namespace ZarządzanieFinansami;
+
+public class Transaction
+    {
+        public string Category { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+
+        public Transaction(string category, decimal amount, DateTime date, string description)
+        {
+            Category = category;
+            Amount = amount;
+            Date = date;
+            Description = description;
+        }
+    }
+
