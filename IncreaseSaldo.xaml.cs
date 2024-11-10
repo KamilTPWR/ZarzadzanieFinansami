1ee21234f604880c7a70c02dc22080cfec8220ac
KamilTPWR <277985@student.pwr.edu.pl>
2024-11-10 15:31:29 +0100

    Działający przycisk i nowe okno PopUp

---
+++ IncreaseSaldo.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace ZarządzanieFinansami;
+
+public partial class IncreaseSaldo : Window
+{
+    public IncreaseSaldo()
+    {
+        InitializeComponent();
+    }
+    private void DodajButton_Click(object sender, RoutedEventArgs e)
+    {
+        // Get the values from TextBoxes
+        string nazwa = NazwaTextBox.Text;
+        string kwotaText = KwotaTextBox.Text;
+            
+        // Try to parse the kwota input as a float
+        if (double.TryParse(kwotaText, out double kwota))
+        {
+            Close();
+            //MessageBox.Show($"Nazwa: {nazwa}\nKwota: {kwota}\nUwagi: {UwagiTextBox.Text}");
+        }
+        else
+        {
+            MessageBox.Show("Invalid input for Kwota. Please enter a numeric value.");
+        }
+    }
+    private void AnulujButton_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Media;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -17,12 +18,18 @@ namespace ZarządzanieFinansami;
 /// </summary>
 public partial class MainWindow : Window
 {
+    Core core = new Core();
     public MainWindow()
-    {
+    {   
         InitializeComponent();
         StartClock();
 
-        this.ResultTextDisplay.Text = "Some string";
+        ChangeSaldoEvent(0);
+        
+        ResultTextDisplay.Text = $"Saldo: {core.saldo} $";
+        
+        //Napisy domyśne
+        SystemClock.Text = "00/00/0000 00:00:00";
         
         //Zarządzanie zegarem w prawym górnym rogu
         void StartClock()
@@ -37,4 +44,45 @@ public partial class MainWindow : Window
             SystemClock.Text = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
         }
     }
+    private void ListView_SizeChanged(object sender, SizeChangedEventArgs e)
+    {
+        var scaleRation = 0.20;
+        var gridView = MyListView.View as GridView;
+
+        double totalWidth = MyListView.ActualWidth - SystemParameters.VerticalScrollBarWidth;
+
+        gridView.Columns[0].Width = totalWidth * scaleRation;  // "Saldo"
+        gridView.Columns[1].Width = totalWidth * scaleRation;  // "Zmiana"
+        gridView.Columns[2].Width = totalWidth * scaleRation;  // "Data"
+        gridView.Columns[3].Width = totalWidth * 2*(scaleRation + 0.01); // "Uwagi"
+        
+    }
+    private void Button_OnClick(object sender, RoutedEventArgs e)
+    {
+        // ChangeSaldoEvent(core.saldo+1);
+        
+        IncreaseSaldo increaseSaldo = new IncreaseSaldo();
+        increaseSaldo.Show();
+        
+    }
+    private void ChangeSaldoEvent(double newSaldo)
+    {
+        core.ChangeSaldo(newSaldo);
+        ResultTextDisplay.Text = $"Saldo: {core.saldo} $";
+    }
+}
+
+public class Core
+{
+    public double saldo { get; set; }
+
+    public Core()
+    {
+        saldo = 0;
+    }
+    public void ChangeSaldo(double newValue)
+    {
+        saldo = newValue;
+    }
+
 }
